6399509e1eaf2a810a5883fd437b5c68085aa9c7
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-03-19 22:09:55 +0000

    setup auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Register } from './features/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // landing page
+
+  // register
+  { path: 'auth/register', component: Register },
+];
