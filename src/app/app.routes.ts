6de6c274450c2fb5ef8ba63748c2331203b27bb2
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-03-29 00:28:31 +0000

    renameFile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
   // landing page
 
   // register
   { path: 'auth/register', component: RegisterComponent },
+  { path: 'auth/login', component: LoginComponent },
 ];
