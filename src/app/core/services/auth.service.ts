bbb6ccd7de2386eafe085d1e7661271e01d1f303
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-04 14:59:16 +0000

    set up login method

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,19 +1,25 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, throwError } from 'rxjs';
+import { UserRole } from '../constant/roles';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = environment.apiUrl;
+  private apiUrl = `${environment.apiUrl}/auth`;
   private http = inject(HttpClient);
+  private route = inject(Router);
   private token!: string;
 
-  getError!: string;
-  load: boolean = false;
+  private getError: string = '';
+  private load: boolean = false;
+
+  user = signal<any | null | undefined>(''); // create user model
   // register
   register(data: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/auth/register`, data).pipe(
+    return this.http.post<any>(`${this.apiUrl}/register`, data).pipe(
       tap({
         // get error from the API
         error: (err) => {
@@ -25,22 +31,60 @@ export class AuthService {
   }
 
   // login
-  login(data: any): Observable<any> {
+  login(data: any): Observable<string> {
     // console.log(data);
-    return this.http.post<any>(`${this.apiUrl}/auth/login`, data).pipe(
-      tap({
-        error: (err) => {
-          this.getError = err.error.message;
-          console.log(this.getError);
-        },
+    return this.http.post<any>(`${this.apiUrl}/login`, data).pipe(
+      tap((jwtToken) => {
+        this.token = jwtToken.token;
+        localStorage.setItem('token', this.token);
+      }),
+
+      catchError((err) => {
+        this.getError = err?.error?.message ?? err.message ?? 'Unknown error';
+        console.log(this.getError);
+        return throwError(() => err);
       }),
     );
-
-    return data;
   }
 
+  isAuthenticated() {
+    return !!localStorage.getItem('token');
+  }
+  // get user
+  getMe() {
+    return this.http.get<any>(`${this.apiUrl}/me`).pipe(
+      tap((user) => {
+        // console.log(user);
+        this.user.set(user);
+      }),
+      catchError((err) => {
+        this.error = err.error?.message;
+        return throwError(() => err);
+      }),
+    );
+  }
+
+  hasRole() {
+    return this.user().role;
+  }
   // redirect after login
-  redirectAfterLogin() {}
+  redirectAfterLogin() {
+    const userRole: UserRole = this.hasRole();
+    // console.log(userRole);
+    if (userRole === UserRole.CANDIDAT) {
+      return this.route.navigate(['/candidat']);
+    } else if (userRole === UserRole.RECRUTER) {
+      return this.route.navigate(['/recruiter']);
+    } else if (userRole === UserRole.ADMIN) {
+      return this.route.navigate(['/admin']);
+    }
+    return;
+  }
+
+  logout() {
+    this.token = '';
+    localStorage.removeItem('token');
+  }
   error() {
     return this.getError;
   }
