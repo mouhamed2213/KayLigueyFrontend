41ca783bda3770fe338ec8a398e6f7001d143814
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-05 19:53:56 +0000

    public route

---
+++ src/app/features/public/public.route.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+
+export const PUBLIC_ROUTE: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
