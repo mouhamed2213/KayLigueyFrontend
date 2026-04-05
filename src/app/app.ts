f60e7ca1fed894fe6f61784566296290fd29daad
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-05 14:49:53 +0000

    add user state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AuthService } from './core/services/auth.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('kay_liguey_front');
+
+  private authService = inject(AuthService);
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.authService.getMe().subscribe();
+    }
+  }
 }
