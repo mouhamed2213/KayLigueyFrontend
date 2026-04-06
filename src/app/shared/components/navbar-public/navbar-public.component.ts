92f6d470a766b582349bf1f81d654bf17d6d0fd1
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-06 00:48:49 +0000

    add nav bar

---
+++ src/app/shared/components/navbar-public/navbar-public.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-public',
+  imports: [],
+  templateUrl: './navbar-public.component.html',
+  styleUrl: './navbar-public.component.css',
+})
+export class NavbarPublicComponent {
+
+}
