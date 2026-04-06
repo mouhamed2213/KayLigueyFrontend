dbb437bfdd88aeb5a569c3492f86e15e883f314c
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-06 20:24:19 +0000

    nav bar

--- src/app/shared/components/navbar-public/navbar-public.component.ts
+++ src/app/shared/components/navbar-public/navbar-public.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-public',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar-public.component.html',
   styleUrl: './navbar-public.component.css',
 })
 export class NavbarPublicComponent {
+  private route = inject(Router);
 
+  isActive(route: string): boolean {
+    if (this.route.url === route) {
+      console.log('true');
+    }
+    return true;
+  }
 }
