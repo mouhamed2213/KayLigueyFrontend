6c64b7976da1a7236ab7e1a626b9e3a0e7602e8f
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-06 23:57:50 +0000

    WIP

--- src/app/shared/components/navbar-public/navbar-public.component.ts
+++ src/app/shared/components/navbar-public/navbar-public.component.ts
@@ -1,7 +1,6 @@
 import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { Router } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
@@ -10,17 +9,4 @@ import { ButtonComponent } from '../button/button.component';
   templateUrl: './navbar-public.component.html',
   styleUrl: './navbar-public.component.css',
 })
-export class NavbarPublicComponent {
-  private route = inject(Router);
-
-  isActive(): string {
-    return `text-kl-red-light border-kl-red-light`;
-  }
-  get class(): string {
-    return `
-      text-kl-muted font-headline hover:border-kl-red-light 
-      hover:text-kl-red-light border-b-2 border-transparent 
-      p-1 font-semibold transition duration-150 ease-in-out hover:scale-105
-`;
-  }
-}
+export class NavbarPublicComponent {}
