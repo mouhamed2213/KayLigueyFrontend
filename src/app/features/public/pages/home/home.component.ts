2896571ecdd2e9727c7a97293f092690344c0395
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-11 00:47:42 +0000

    HOME ROLE CARD

--- src/app/features/public/pages/home/home.component.ts
+++ src/app/features/public/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { LucideAngularModule } from 'lucide-angular';
 import { CardsComponent } from '../../../../shared/components/cards/cards.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [LucideAngularModule, ButtonComponent, CardsComponent],
+  imports: [LucideAngularModule, ButtonComponent, CardsComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
