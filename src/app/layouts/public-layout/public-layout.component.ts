2f836cf5af0b20c76a83d7e136bf8b668760b45a
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-13 23:02:21 +0000

    kyliigueye footer

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarPublicComponent } from '../../shared/components/navbar-public/navbar-public.component';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from "../../shared/components/button/button.component";
 
 @Component({
   selector: 'app-public-layout',
-  imports: [NavbarPublicComponent, RouterOutlet],
+  imports: [NavbarPublicComponent, RouterOutlet, ButtonComponent],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css',
 })
