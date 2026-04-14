997f9b22c9de19e5ca7edd0b94390ec4b1d0d793
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-14 20:17:21 +0000

    refactore

--- src/app/features/public/pages/contact/contact.component.ts
+++ src/app/features/public/pages/contact/contact.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { LucideAngularModule } from 'lucide-angular';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { InputComponent } from '../../../../shared/components/input/input.component';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [LucideAngularModule, ButtonComponent, InputComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
