8de4723148a728576e1ae9456f986d17deccf9d3
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-17 14:55:18 +0000

    job offer model

---
+++ src/app/core/models/job_offer.model.ts
@@ -0,0 +1,31 @@
+import { ContractType, WorkingMode, JobStatus } from '../constant/enums';
+
+export interface JobOffer {
+  company_id?: string;
+  title: string;
+  description: string;
+  profile_required?: string | null;
+
+  contract_type: ContractType;
+  working_mode: WorkingMode;
+
+  city?: string | null;
+
+  salary_min?: number | ;
+  salary_max?: number | null;
+
+  experience_years?: number | null;
+  education_level?: string | null;
+
+  status: JobStatus;
+  is_featured: boolean;
+
+  published_at?: string | null; // ISO string
+  expires_at?: string | null;
+
+  view_count: number;
+  application_count: number;
+
+  created_at: string;
+  updated_at: string;
+}
