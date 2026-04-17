dcfc26316c5e6a12b52fd0110c860b3222f07e15
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-17 22:04:43 +0000

    refactore : add takeuntilDestroyed

--- src/app/features/public/pages/offres/offres.component.ts
+++ src/app/features/public/pages/offres/offres.component.ts
@@ -1,24 +1,36 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { JobOfferService } from '../../../../core/services/job_offer.service';
 import { JobOffer } from '../../../../core/models/job_offer.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
+import { DestroyRef } from '@angular/core';
+
 @Component({
   selector: 'app-offres',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './offres.component.html',
   styleUrl: './offres.component.css',
 })
 export class OffresComponent implements OnInit {
   private jobOfferService = inject(JobOfferService);
-  private jobOffers: JobOffer[] = [];
+  protected jobOffers = signal<JobOffer[]>([]);
+  protected isLoading = signal(true);
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    this.jobOfferService.jobOffer().subscribe({
-      next(value) {
-        console.log('GOT IT ', value);
-      },
-      error(err) {
-        console.log(err);
-      },
-    });
+    this.jobOfferService
+      .jobOffer()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: ({ data }) => {
+          console.log(data);
+          this.jobOffers.set(data);
+          this.isLoading.set(true);
+        },
+        error(err) {
+          // this.isLoading.set(false);
+          console.log(err);
+        },
+      });
   }
 }
