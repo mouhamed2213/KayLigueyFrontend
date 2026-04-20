fa08481e1297948a03aa4a1221b5c9b2790c5b20
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-20 07:41:27 +0000

    offer api response interface

---
+++ src/app/core/models/offer_respose.model.ts
@@ -0,0 +1,13 @@
+// APi Response
+export interface IOfferResponse<T> {
+  success?: boolean;
+  data: T;
+  meta: IPagination;
+}
+
+export interface IPagination {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+}
