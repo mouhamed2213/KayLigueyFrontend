934fe6f0be78b2f3307b584a214acb912a08d65d
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-24 19:24:36 +0000

    create : Candidate component

---
+++ src/app/shared/components/candidate-nav/candidate-nav.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CandidateNavComponent } from './candidate-nav.component';
+
+describe('CandidateNavComponent', () => {
+  let component: CandidateNavComponent;
+  let fixture: ComponentFixture<CandidateNavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CandidateNavComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CandidateNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/candidate-nav/candidate-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-nav',
+  imports: [],
+  templateUrl: './candidate-nav.component.html',
+  styleUrl: './candidate-nav.component.css',
+})
+export class CandidateNavComponent {
+
+}
