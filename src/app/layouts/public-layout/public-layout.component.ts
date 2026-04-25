866fa7b455c1f076d577f34ccf0d42fb16af7b6a
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-04-25 09:16:44 +0000

    controle public layout display

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { NavbarPublicComponent } from '../../shared/components/navbar-public/navbar-public.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
- @Component({
+import { CandidateNavComponent } from '../../shared/components/candidate-nav/candidate-nav.component';
+import { AuthService } from '@core/services/auth.service';
+
+@Component({
   selector: 'app-public-layout',
-  imports: [NavbarPublicComponent, RouterOutlet, FooterComponent],
+
+  imports: [
+    NavbarPublicComponent,
+    RouterOutlet,
+    FooterComponent,
+    CandidateNavComponent,
+  ],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css',
 })
-export class PublicLayoutComponent {}
+export class PublicLayoutComponent {
+  protected authservice = inject(AuthService);
+  protected state = signal<boolean>(this.authservice.isAuthenticated());
+
+  protected isAuthenticated = computed(() => {
+    console.log(this.state());
+    return this.state();
+  });
+}
